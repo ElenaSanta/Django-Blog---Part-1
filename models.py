1325b332d98fbfd9c0c600a48d5fa890b9379327
ElenaSanta <elenasanta51@gmail.com>
2026-05-27 10:30:09 +0300

    Add files via upload

---
+++ models.py
@@ -0,0 +1,16 @@
+from django.db import models
+from django.conf import settings
+
+class Post(models.Model):
+    class Status(models.TextChoices):
+        DRAFT = 'DF', 'Draft'
+        PUBLISHED = 'P', 'Published'
+
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    title = models.CharField(max_length=250)
+    slug = models.SlugField(max_length=250)
+    body = models.TextField()
+    publish = models.DateTimeField()
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    status = models.CharField(max_length=2, choices=Status.choices, default=Status.DRAFT)
